b28e2ad65ebda8b80eabdf6683a9c9589c9cf755
Claudio Moreno <strife_dev@outlook.com>
2021-12-29 11:58:40 -0600

    adding env variables for URL

--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -9,7 +9,7 @@ const VerCliente = () => {
   const { nombre, email, telefono, empresa, notas } = cliente;
   const ObtenerClienteApi = async () => {
     try {
-      const url = `http://localhost:4000/clientes/${id}`;
+      const url = `${import.meta.env.VITE_API_URL}/${id}`;
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
       setCliente(resultado);
